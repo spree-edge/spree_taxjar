924e2d89690f1b7cecaba1b250c7077bd60effd8
Nathaniel Nande <nathaniel.nande@sparksolutions.co>
2020-04-15 14:28:08 +0200

    Introduce taxjar_sandbox_environment_enabled spree preference

--- app/controllers/spree/admin/taxjar_settings_controller.rb
+++ app/controllers/spree/admin/taxjar_settings_controller.rb
@@ -2,13 +2,14 @@ module Spree
   module Admin
     class TaxjarSettingsController < Spree::Admin::BaseController
       def edit
-        @preferences_api = [:taxjar_api_key, :taxjar_enabled, :taxjar_debug_enabled]
+        @preferences_api = %i[taxjar_api_key taxjar_enabled taxjar_debug_enabled taxjar_sandbox_environment_enabled]
       end
 
       def update
         Spree::Config[:taxjar_api_key] = params[:taxjar_api_key]
         Spree::Config[:taxjar_enabled] = params[:taxjar_enabled]
         Spree::Config[:taxjar_debug_enabled] = params[:taxjar_debug_enabled]
+        Spree::Config[:taxjar_sandbox_environment_enabled] = params[:taxjar_sandbox_environment_enabled]
 
         flash[:success] = Spree.t(:taxjar_settings_updated)
         redirect_to edit_admin_taxjar_settings_path
